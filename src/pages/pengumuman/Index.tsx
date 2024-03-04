4e164e8271b7941a43eebde8c049117fc53c7c3f
ThomasAlberto21 <thomasalberto456@gmail.com>
2024-03-04 19:29:42 +0700

    index

--- src/pages/pengumuman/Index.tsx
+++ src/pages/pengumuman/Index.tsx
@@ -1,15 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { debounce } from 'lodash';
 import { useDispatch } from 'react-redux';
 import { setPageTitle } from '../../store/themeConfigSlice';
+import { downloadExcel } from 'react-export-table-to-excel';
 import { requestGetPengumuman } from '../../api/pengumuman/services/requestGetPengumuman';
-import { debounce } from 'lodash';
-import BreadcrumbsDefault from '../../components/breadcrumbs/BreadcrumbsDefault';
-import SearchBasic from '../../components/searchs/SearchBasic';
-import { Link } from 'react-router-dom';
-import TippyDefault from '../../components/tippys/default/TippyDefault';
-import ButtonIcon from '../../components/buttons/icon/ButtonIcon';
 import Table from './Table/Index';
-
+import ButtonIcon from '../../components/buttons/icon/ButtonIcon';
+import SearchBasic from '../../components/searchs/SearchBasic';
+import TippyDefault from '../../components/tippys/default/TippyDefault';
+import BreadcrumbsDefault from '../../components/breadcrumbs/BreadcrumbsDefault';
 interface PengumumanList {
   id_pengumuman: string;
   pendaftaran: {
@@ -59,6 +59,32 @@ const Pengumuman: React.FunctionComponent = () => {
     filterPengumumanList(query);
   };
 
+  const recordLaporanPengumuman = () => {
+    return initialPengumumanList.map((item: any, index: number) => {
+      return {
+        index: index + 1,
+        nama_lengkap: item?.pendaftaran?.nama_lengkap,
+        email: item?.pendaftaran?.email,
+        kursus: item?.pendaftaran?.kursus?.nama_kursus,
+        nilai_test: item?.nilai_test,
+        nilai_interview: item?.nilai_interview,
+        nilai_rata_rata: item?.nilai_rata_rata,
+        hasil_pengumuman: item?.hasil_pengumuman,
+      };
+    });
+  };
+
+  const handlePrintLaporanPengumuman = () => {
+    downloadExcel({
+      fileName: 'Laporan Pengumuman Pendaftar Kursus',
+      sheet: 'react-export-table-to-excel',
+      tablePayload: {
+        header: ['No', 'Nama Lengkap', 'Email Pendaftar', 'Kursus Yang Dipilih', 'Nilai Test', 'Nilai Interview', 'Nilai Rata-Rata', 'Hasil Pengumuman'],
+        body: recordLaporanPengumuman(),
+      },
+    });
+  };
+
   const handleRefresh = () => {
     window.location.reload();
   };
@@ -84,7 +110,9 @@ const Pengumuman: React.FunctionComponent = () => {
               <ButtonIcon icon="ic:baseline-plus" backgroundColor="btn-primary" />
             </TippyDefault>
           </Link>
-
+          <TippyDefault content="Cetak Laporan Barang Masuk">
+            <ButtonIcon icon="mdi:printer" backgroundColor="btn-success" onClick={handlePrintLaporanPengumuman} />
+          </TippyDefault>
           <TippyDefault content="Refresh Halaman">
             <ButtonIcon icon="material-symbols:refresh" backgroundColor="btn-info" onClick={handleRefresh} />
           </TippyDefault>
